3b458d30428884842c6955c84cabe67d8bc68d41
Oderinde Suliha <oderindesuliha@gmail.com>
2025-08-04 14:31:51 +0100

    C#

---
+++ BackToSenderLogistic/Back2Sender.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Back2Sender {
+    static void Main(string[] args) {
+        Console.Write("Enter Collection rate: ");
+        int rate = int.Parse(Console.ReadLine());
+
+        if (rate <= 0) {
+            Console.WriteLine("Invalid rate");
+        }
+
+        double result = CalculatePay(rate);
+        Console.WriteLine("The total pay for " + rate + " successful delivery is: " + result);
+        
+    }
+
+    public static double CalculatePay(int rate) {
+        if (rate < 50) {
+            return rate * 160 + 5000;
+        }
+        else if (rate <= 59) {
+            return rate * 200 + 5000;
+        }
+        else if (rate <= 69) {
+            return rate * 250 + 5000;
+        }
+        else {
+            return rate * 500 + 5000;
+        }
+    }
+
+
+}
---
+++ Functions/WeekendSnacks.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class WeekendSnacks {
+    static void Main(string[] args)
+    {
+        Console.Write("Enter a number: ");
+        int number = int.Parse(Console.ReadLine());
+        
+        
+        bool even = isEven(number);
+        Console.Write(even);
+
+    }
+
+    public static bool isEven(int number) {
+        if (number % 2 == 0)
+        {
+            return true;
+        }
+
+        return false;
+
+    }
+    
+}
